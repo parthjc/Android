bd3c4929d778166d7e0a1f620bbb2a5212cead88
parthjc <109025555+parthjc@users.noreply.github.com>
2023-03-05 18:52:01 +0530

    Add files via upload

--- MyApplication/app/src/main/java/com/example/myapplication/activity_singup.java
+++ MyApplication/app/src/main/java/com/example/myapplication/activity_singup.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
 public class activity_singup extends AppCompatActivity {
 
     @Override
