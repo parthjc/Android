95d05af19a3a8b562bd38934c1995e10a3dff547
Parth Chaudhari <chaudhariparth454@gmail.com>
2023-05-17 22:45:11 +0530

    frgmt

---
+++ MyApplication/app/src/main/java/com/example/myapplication/activity_frgmt_second.java
@@ -0,0 +1,31 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.myapplication.databinding.ActivityFrgmtSecondBinding;
+
+public class activity_frgmt_second extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frgmt_second);
+
+
+
+
+    }
+
+}
