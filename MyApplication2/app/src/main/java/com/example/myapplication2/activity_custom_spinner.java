e59e29d9e0035c143b6307aeedd3e7068fdf2922
Parth Chaudhari <chaudhariparth454@gmail.com>
2023-05-19 16:08:36 +0530

    darkmode

--- MyApplication2/app/src/main/java/com/example/myapplication2/activity_custom_spinner.java
+++ MyApplication2/app/src/main/java/com/example/myapplication2/activity_custom_spinner.java
@@ -1,8 +1,15 @@
 package com.example.myapplication2;
 
+import static com.example.myapplication2.R.layout.spinner_selected_text;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class activity_custom_spinner extends AppCompatActivity {
 
@@ -10,5 +17,64 @@ public class activity_custom_spinner extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_spinner);
-    }
-}
+
+
+        String data[] = {"Java", "Python", "C++", "C", "Android", "JavaScript"};
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, spinner_selected_text, data);
+        adapter.setDropDownViewResource(R.layout.spinner_dropdown);
+
+        Spinner spinner = findViewById(R.id.spinner);
+
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+                Toast.makeText(activity_custom_spinner.this, adapterView.getItemAtPosition(position).toString(), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+
+
+
+
+    }//oncreate
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}//class
